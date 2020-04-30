77945a32a48a0532fbb7571ca05b8ae75ed19287
juliomchado <juliocarlos00@hotmail.com>
2020-04-30 16:56:21 +0100

    ToggleMenu completo

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+
+import './App.css'
 
 function App() {
+
+  function toggleMenu() {
+    const menu = document.querySelector('.menu')
+
+    menu.classList.toggle('active')
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <React.Fragment>
+      <div className="container">
+        <div className="menu">
+          <h1 className="h1" onClick={toggleMenu}>X</h1>
+          <ul>
+            <li><a href="aa" className="a">PRINCIPAL</a></li>
+            <li><a href="an" className="a">INFO</a></li>
+            <li><a href="am" className="a">ACCOUNT</a></li>
+            <li><a href="al" className="a">CONFIG</a></li>
+          </ul>
+        </div>
+        <div className="button-container">
+          <span className="button-toggle" onClick={toggleMenu}></span>
+          <span className="button-toggle" onClick={toggleMenu}></span>
+          <span className="button-toggle" onClick={toggleMenu}></span>
+        </div>
+      </div>
+    </React.Fragment>
   );
 }
 
